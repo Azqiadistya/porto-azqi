468e2769f0c04b162b9dc6b29a34e9f4b0a1031b
Azqiadistya <azqiadistya.aa@gmail.com>
2024-02-10 15:52:37 +0700

    solve slide button roadmap

--- src/app/pages/roadmap.tsx
+++ src/app/pages/roadmap.tsx
@@ -1,4 +1,5 @@
 // pages/about/about.tsx
+"use client"
 import React from "react";
 import Image from 'next/image'
 import { FaLaravel } from "react-icons/fa6";
@@ -7,27 +8,44 @@ import { BiLogoPhp } from "react-icons/bi";
 import { BsFillBootstrapFill } from "react-icons/bs";
 import RoadmapFe from "./roadmapfe";
 import RoadmapReact from "./roadmapreact";
+import { useState } from 'react';
 
 function RoadmapPage() {
+  const [showComponentFe, setShowComponentFe] = useState(true);
+  const [showComponentReact, setShowComponentReact] = useState(false);
+  const [activeButton, setActiveButton] = useState('Fe');
+
+  const handleClickFe = () => {
+    setShowComponentFe(true);
+    setShowComponentReact(false);
+    setActiveButton('Fe');
+  };
+
+  const handleClickReact = () => {
+    setShowComponentFe(false);
+    setShowComponentReact(true);
+    setActiveButton('React');
+  };
+
   return (
-    <><div className="grid grid-cols-5 px-20 pt-20 pb-2 bg-white" id="roadmap">
-      <div className="col-span-3">
-        <h1 className="font-['Rajdhani'] text-lg font-semibold pb-1 text-blue-950">- Roadmap</h1>
-        <h1 className="font-['Rajdhani'] text-5xl font-black text-blue-950">Learning path recomendation</h1>
+    <>
+      <div className="grid grid-cols-5 px-20 pt-20 pb-2 bg-white" id="roadmap">
+        <div className="col-span-3">
+          <h1 className="font-['Rajdhani'] text-lg font-semibold pb-1 text-blue-950">- Roadmap</h1>
+          <h1 className="font-['Rajdhani'] text-5xl font-black text-blue-950">Learning path recomendation</h1>
+        </div>
       </div>
-    </div>
-    <div className="grid grid-cols-4 gap-5 px-20 pb-3 bg-white">
-        <button className="bg-blue-950 hover:bg-blue-900 text-white text-sm font-bold py-3 px-1 rounded-full w-full">
+      <div className="grid grid-cols-4 gap-5 px-20 pb-3 bg-white">
+        <button onClick={handleClickFe} className={`bg-${activeButton === 'Fe' ? 'blue-900' : 'gray-400'} hover:bg-blue-950 text-white text-sm font-bold py-3 px-1 rounded-full w-full`}>
           Frontend Developer
         </button>       
-        <button className="bg-gray-400 hover:bg-blue-950 text-white text-sm font-bold py-3 rounded-full w-full">
+        <button onClick={handleClickReact} className={`bg-${activeButton === 'React' ? 'blue-900' : 'gray-400'} hover:bg-blue-950 text-white text-sm font-bold py-3 rounded-full w-full`}>
           Mastering React Js
         </button>       
-    </div >
-    <RoadmapFe /> 
-    {/* <RoadmapReact />  */}
+      </div>
+      {showComponentFe && <RoadmapFe />}
+      {showComponentReact && <RoadmapReact />}
     </> 
-    
   );
 }
 
--- src/app/pages/roadmapreact.tsx
+++ src/app/pages/roadmapreact.tsx
@@ -19,232 +19,232 @@ import { SiReacthookform } from "react-icons/si";
 function RoadmapReact() {
   return (
     <div className="grid grid-cols-2 px-20 bg-white flex">
-        <div className="flex flex-wrap mt-5 bg-white col-span-2 rounded-full p-3 justify-between border-4 border-blue-100">
+        <div className="flex flex-wrap mt-5 bg-white col-span-2 rounded-full p-3 justify-between border-4 border-blue-100 hover:bg-slate-200">
             <div className="flex flex-wrap pl-5">
                 <BiLogoReact size={32} color="orange" />
                 <p className="font-['Rajdhani'] text-lg font-bold text-blue-950 pl-3">Fundamental of React</p>
             </div> 
             <div className="flex pr-5">
                 <a href="https://www.youtube.com/watch?v=NBZ9Ro6UKV8&list=PLFIM0718LjIVuONHysfOK0ZtiqUWvrx4F&ab_channel=WebProgrammingUNPAS" className="flex">
-                    <button className="bg-gray-400 hover:bg-gray-250 text-white text-sm font-bold py-2 px-4 rounded-full">
+                    <button className="bg-gray-400 hover:bg-blue-900 text-white text-sm font-bold py-2 px-4 rounded-full">
                         Indonesia
                     </button>                      
                 </a>   
                 <a href="https://www.youtube.com/watch?v=NBZ9Ro6UKV8&list=PLFIM0718LjIVuONHysfOK0ZtiqUWvrx4F&ab_channel=WebProgrammingUNPAS" className="flex pl-2">
-                    <button className="bg-gray-400 hover:bg-gray-250 text-white text-sm font-bold py-2 px-4 rounded-full">
+                    <button className="bg-gray-400 hover:bg-blue-900 text-white text-sm font-bold py-2 px-4 rounded-full">
                         English
                     </button>                      
                 </a>   
             </div>
         </div>
-        <div className="flex flex-wrap mt-3 bg-white col-span-2 rounded-full p-3 justify-between border-4 border-blue-100">
+        <div className="flex flex-wrap mt-3 bg-white col-span-2 rounded-full p-3 justify-between border-4 border-blue-100 hover:bg-slate-200">
             <div className="flex flex-wrap pl-5">
                 <BiLogoReact size={32} color="orange" />
                 <p className="font-['Rajdhani'] text-lg font-bold text-blue-950 pl-3">React Hooks</p>
             </div> 
             <div className="flex pr-5">
                 <a href="https://www.youtube.com/watch?v=NBZ9Ro6UKV8&list=PLFIM0718LjIVuONHysfOK0ZtiqUWvrx4F&ab_channel=WebProgrammingUNPAS" className="flex">
-                    <button className="bg-gray-400 hover:bg-gray-250 text-white text-sm font-bold py-2 px-4 rounded-full">
+                    <button className="bg-gray-400 hover:bg-blue-900 text-white text-sm font-bold py-2 px-4 rounded-full">
                         English
                     </button>                      
                 </a>   
             </div>
         </div>
-        <div className="flex flex-wrap mt-3 bg-white col-span-2 rounded-full p-3 justify-between border-4 border-blue-100">
+        <div className="flex flex-wrap mt-3 bg-white col-span-2 rounded-full p-3 justify-between border-4 border-blue-100 hover:bg-slate-200">
             <div className="flex flex-wrap pl-5">
                 <SiReactrouter size={32} color="orange" />
                 <p className="font-['Rajdhani'] text-lg font-bold text-blue-950 pl-3">React Router</p>
             </div> 
             <div className="flex pr-5">
                 <a href="https://www.youtube.com/watch?v=NBZ9Ro6UKV8&list=PLFIM0718LjIVuONHysfOK0ZtiqUWvrx4F&ab_channel=WebProgrammingUNPAS" className="flex">
-                    <button className="bg-gray-400 hover:bg-gray-250 text-white text-sm font-bold py-2 px-4 rounded-full">
+                    <button className="bg-gray-400 hover:bg-blue-900 text-white text-sm font-bold py-2 px-4 rounded-full">
                         English
                     </button>                      
                 </a>   
             </div>
         </div>
-        <div className="flex flex-wrap mt-3 bg-white col-span-2 rounded-full p-3 justify-between border-4 border-blue-100">
+        <div className="flex flex-wrap mt-3 bg-white col-span-2 rounded-full p-3 justify-between border-4 border-blue-100 hover:bg-slate-200">
             <div className="flex flex-wrap pl-5">
                 <SiRedux size={32} color="orange" />
                 <p className="font-['Rajdhani'] text-lg font-bold text-blue-950 pl-3">React Redux</p>
             </div> 
             <div className="flex pr-5">
                 <a href="https://www.youtube.com/watch?v=NBZ9Ro6UKV8&list=PLFIM0718LjIVuONHysfOK0ZtiqUWvrx4F&ab_channel=WebProgrammingUNPAS" className="flex">
-                    <button className="bg-gray-400 hover:bg-gray-250 text-white text-sm font-bold py-2 px-4 rounded-full">
+                    <button className="bg-gray-400 hover:bg-blue-900 text-white text-sm font-bold py-2 px-4 rounded-full">
                         English
                     </button>                      
                 </a>   
             </div>
         </div>
-        <div className="flex flex-wrap mt-3 bg-white col-span-2 rounded-full p-3 justify-between border-4 border-blue-100">
+        <div className="flex flex-wrap mt-3 bg-white col-span-2 rounded-full p-3 justify-between border-4 border-blue-100 hover:bg-slate-200">
             <div className="flex flex-wrap pl-5">
                 <BiLogoReact size={32} color="orange" />
                 <p className="font-['Rajdhani'] text-lg font-bold text-blue-950 pl-3">Practical React</p>
             </div> 
             <div className="flex pr-5">
                 <a href="https://www.youtube.com/watch?v=NBZ9Ro6UKV8&list=PLFIM0718LjIVuONHysfOK0ZtiqUWvrx4F&ab_channel=WebProgrammingUNPAS" className="flex">
-                    <button className="bg-gray-400 hover:bg-gray-250 text-white text-sm font-bold py-2 px-4 rounded-full">
+                    <button className="bg-gray-400 hover:bg-blue-900 text-white text-sm font-bold py-2 px-4 rounded-full">
                         English
                     </button>                      
                 </a>   
             </div>
         </div>
-        <div className="flex flex-wrap mt-3 bg-white col-span-2 rounded-full p-3 justify-between border-4 border-blue-100">
+        <div className="flex flex-wrap mt-3 bg-white col-span-2 rounded-full p-3 justify-between border-4 border-blue-100 hover:bg-slate-200">
             <div className="flex flex-wrap pl-5">
                 <BiLogoReact size={32} color="orange" />
                 <p className="font-['Rajdhani'] text-lg font-bold text-blue-950 pl-3">React Formik</p>
             </div> 
             <div className="flex pr-5">
                 <a href="https://www.youtube.com/watch?v=NBZ9Ro6UKV8&list=PLFIM0718LjIVuONHysfOK0ZtiqUWvrx4F&ab_channel=WebProgrammingUNPAS" className="flex">
-                    <button className="bg-gray-400 hover:bg-gray-250 text-white text-sm font-bold py-2 px-4 rounded-full">
+                    <button className="bg-gray-400 hover:bg-blue-900 text-white text-sm font-bold py-2 px-4 rounded-full">
                         English
                     </button>                      
                 </a>   
             </div>
         </div>
-        <div className="flex flex-wrap mt-3 bg-white col-span-2 rounded-full p-3 justify-between border-4 border-blue-100">
+        <div className="flex flex-wrap mt-3 bg-white col-span-2 rounded-full p-3 justify-between border-4 border-blue-100 hover:bg-slate-200">
             <div className="flex flex-wrap pl-5">
                 <SiStorybook size={32} color="orange" />
                 <p className="font-['Rajdhani'] text-lg font-bold text-blue-950 pl-3">React Storybook</p>
             </div> 
             <div className="flex pr-5">
                 <a href="https://www.youtube.com/watch?v=NBZ9Ro6UKV8&list=PLFIM0718LjIVuONHysfOK0ZtiqUWvrx4F&ab_channel=WebProgrammingUNPAS" className="flex">
-                    <button className="bg-gray-400 hover:bg-gray-250 text-white text-sm font-bold py-2 px-4 rounded-full">
+                    <button className="bg-gray-400 hover:bg-blue-900 text-white text-sm font-bold py-2 px-4 rounded-full">
                         English
                     </button>                      
                 </a>   
             </div>
         </div>
-        <div className="flex flex-wrap mt-3 bg-white col-span-2 rounded-full p-3 justify-between border-4 border-blue-100">
+        <div className="flex flex-wrap mt-3 bg-white col-span-2 rounded-full p-3 justify-between border-4 border-blue-100 hover:bg-slate-200">
             <div className="flex flex-wrap pl-5">
                 <BiLogoReact size={32} color="orange" />
                 <p className="font-['Rajdhani'] text-lg font-bold text-blue-950 pl-3">React Render</p>
             </div> 
             <div className="flex pr-5">
                 <a href="https://www.youtube.com/watch?v=NBZ9Ro6UKV8&list=PLFIM0718LjIVuONHysfOK0ZtiqUWvrx4F&ab_channel=WebProgrammingUNPAS" className="flex">
-                    <button className="bg-gray-400 hover:bg-gray-250 text-white text-sm font-bold py-2 px-4 rounded-full">
+                    <button className="bg-gray-400 hover:bg-blue-900 text-white text-sm font-bold py-2 px-4 rounded-full">
                         English
                     </button>                      
                 </a>   
             </div>
         </div>
-        <div className="flex flex-wrap mt-3 bg-white col-span-2 rounded-full p-3 justify-between border-4 border-blue-100">
+        <div className="flex flex-wrap mt-3 bg-white col-span-2 rounded-full p-3 justify-between border-4 border-blue-100 hover:bg-slate-200">
             <div className="flex flex-wrap pl-5">
                 <SiReacttable size={32} color="orange" />
                 <p className="font-['Rajdhani'] text-lg font-bold text-blue-950 pl-3">React Table</p>
             </div> 
             <div className="flex pr-5">
                 <a href="https://www.youtube.com/watch?v=NBZ9Ro6UKV8&list=PLFIM0718LjIVuONHysfOK0ZtiqUWvrx4F&ab_channel=WebProgrammingUNPAS" className="flex">
-                    <button className="bg-gray-400 hover:bg-gray-250 text-white text-sm font-bold py-2 px-4 rounded-full">
+                    <button className="bg-gray-400 hover:bg-blue-900 text-white text-sm font-bold py-2 px-4 rounded-full">
                         English
                     </button>                      
                 </a>   
             </div>
         </div>
-        <div className="flex flex-wrap mt-3 bg-white col-span-2 rounded-full p-3 justify-between border-4 border-blue-100">
+        <div className="flex flex-wrap mt-3 bg-white col-span-2 rounded-full p-3 justify-between border-4 border-blue-100 hover:bg-slate-200">
             <div className="flex flex-wrap pl-5">
                 <BiLogoReact size={32} color="orange" />
                 <p className="font-['Rajdhani'] text-lg font-bold text-blue-950 pl-3">React Typescript</p>
             </div> 
             <div className="flex pr-5">
                 <a href="https://www.youtube.com/watch?v=NBZ9Ro6UKV8&list=PLFIM0718LjIVuONHysfOK0ZtiqUWvrx4F&ab_channel=WebProgrammingUNPAS" className="flex">
-                    <button className="bg-gray-400 hover:bg-gray-250 text-white text-sm font-bold py-2 px-4 rounded-full">
+                    <button className="bg-gray-400 hover:bg-blue-900 text-white text-sm font-bold py-2 px-4 rounded-full">
                         English
                     </button>                      
                 </a>   
             </div>
         </div>
-        <div className="flex flex-wrap mt-3 bg-white col-span-2 rounded-full p-3 justify-between border-4 border-blue-100">
+        <div className="flex flex-wrap mt-3 bg-white col-span-2 rounded-full p-3 justify-between border-4 border-blue-100 hover:bg-slate-200">
             <div className="flex flex-wrap pl-5">
                 <SiReactquery size={32} color="orange" />
                 <p className="font-['Rajdhani'] text-lg font-bold text-blue-950 pl-3">React Query</p>
             </div> 
             <div className="flex pr-5">
                 <a href="https://www.youtube.com/watch?v=NBZ9Ro6UKV8&list=PLFIM0718LjIVuONHysfOK0ZtiqUWvrx4F&ab_channel=WebProgrammingUNPAS" className="flex">
-                    <button className="bg-gray-400 hover:bg-gray-250 text-white text-sm font-bold py-2 px-4 rounded-full">
+                    <button className="bg-gray-400 hover:bg-blue-900 text-white text-sm font-bold py-2 px-4 rounded-full">
                         English
                     </button>                      
                 </a>   
             </div>
         </div>
-        <div className="flex flex-wrap mt-3 bg-white col-span-2 rounded-full p-3 justify-between border-4 border-blue-100">
+        <div className="flex flex-wrap mt-3 bg-white col-span-2 rounded-full p-3 justify-between border-4 border-blue-100 hover:bg-slate-200">
             <div className="flex flex-wrap pl-5">
                 <BiLogoReact size={32} color="orange" />
                 <p className="font-['Rajdhani'] text-lg font-bold text-blue-950 pl-3">React Material UI</p>
             </div> 
             <div className="flex pr-5">
                 <a href="https://www.youtube.com/watch?v=NBZ9Ro6UKV8&list=PLFIM0718LjIVuONHysfOK0ZtiqUWvrx4F&ab_channel=WebProgrammingUNPAS" className="flex">
-                    <button className="bg-gray-400 hover:bg-gray-250 text-white text-sm font-bold py-2 px-4 rounded-full">
+                    <button className="bg-gray-400 hover:bg-blue-900 text-white text-sm font-bold py-2 px-4 rounded-full">
                         English
                     </button>                      
                 </a>    
             </div>
         </div>
-        <div className="flex flex-wrap mt-3 bg-white col-span-2 rounded-full p-3 justify-between border-4 border-blue-100">
+        <div className="flex flex-wrap mt-3 bg-white col-span-2 rounded-full p-3 justify-between border-4 border-blue-100 hover:bg-slate-200">
             <div className="flex flex-wrap pl-5">
                 <BiLogoReact size={32} color="orange" />
                 <p className="font-['Rajdhani'] text-lg font-bold text-blue-950 pl-3">React Redux Toolkit</p>
             </div> 
             <div className="flex pr-5">
                 <a href="https://www.youtube.com/watch?v=NBZ9Ro6UKV8&list=PLFIM0718LjIVuONHysfOK0ZtiqUWvrx4F&ab_channel=WebProgrammingUNPAS" className="flex">
-                    <button className="bg-gray-400 hover:bg-gray-250 text-white text-sm font-bold py-2 px-4 rounded-full">
+                    <button className="bg-gray-400 hover:bg-blue-900 text-white text-sm font-bold py-2 px-4 rounded-full">
                         English
                     </button>                      
                 </a>   
             </div>
         </div>
-        <div className="flex flex-wrap mt-3 bg-white col-span-2 rounded-full p-3 justify-between border-4 border-blue-100">
+        <div className="flex flex-wrap mt-3 bg-white col-span-2 rounded-full p-3 justify-between border-4 border-blue-100 hover:bg-slate-200">
             <div className="flex flex-wrap pl-5">
                 <SiReacthookform size={32} color="orange" />
                 <p className="font-['Rajdhani'] text-lg font-bold text-blue-950 pl-3">React Hook Form</p>
             </div> 
             <div className="flex pr-5">
                 <a href="https://www.youtube.com/watch?v=NBZ9Ro6UKV8&list=PLFIM0718LjIVuONHysfOK0ZtiqUWvrx4F&ab_channel=WebProgrammingUNPAS" className="flex">
-                    <button className="bg-gray-400 hover:bg-gray-250 text-white text-sm font-bold py-2 px-4 rounded-full">
+                    <button className="bg-gray-400 hover:bg-blue-900 text-white text-sm font-bold py-2 px-4 rounded-full">
                         English
                     </button>                      
                 </a>   
             </div>
         </div>
-        <div className="flex flex-wrap mt-3 bg-white col-span-2 rounded-full p-3 justify-between border-4 border-blue-100">
+        <div className="flex flex-wrap mt-3 bg-white col-span-2 rounded-full p-3 justify-between border-4 border-blue-100 hover:bg-slate-200">
             <div className="flex flex-wrap pl-5">
                 <BiLogoReact size={32} color="orange" />
                 <p className="font-['Rajdhani'] text-lg font-bold text-blue-950 pl-3">React Testing</p>
             </div> 
             <div className="flex pr-5">
                 <a href="https://www.youtube.com/watch?v=NBZ9Ro6UKV8&list=PLFIM0718LjIVuONHysfOK0ZtiqUWvrx4F&ab_channel=WebProgrammingUNPAS" className="flex">
-                    <button className="bg-gray-400 hover:bg-gray-250 text-white text-sm font-bold py-2 px-4 rounded-full">
+                    <button className="bg-gray-400 hover:bg-blue-900 text-white text-sm font-bold py-2 px-4 rounded-full">
                         English
                     </button>                      
                 </a>   
             </div>
         </div>
-        <div className="flex flex-wrap mt-3 bg-white col-span-2 rounded-full p-3 justify-between border-4 border-blue-100">
+        <div className="flex flex-wrap mt-3 bg-white col-span-2 rounded-full p-3 justify-between border-4 border-blue-100 hover:bg-slate-200">
             <div className="flex flex-wrap pl-5">
                 <BiLogoReact size={32} color="orange" />
                 <p className="font-['Rajdhani'] text-lg font-bold text-blue-950 pl-3">React Native</p>
             </div> 
             <div className="flex pr-5">
                 <a href="https://www.youtube.com/watch?v=NBZ9Ro6UKV8&list=PLFIM0718LjIVuONHysfOK0ZtiqUWvrx4F&ab_channel=WebProgrammingUNPAS" className="flex">
-                    <button className="bg-gray-400 hover:bg-gray-250 text-white text-sm font-bold py-2 px-4 rounded-full">
+                    <button className="bg-gray-400 hover:bg-blue-900 text-white text-sm font-bold py-2 px-4 rounded-full">
                         English
                     </button>                      
                 </a>      
             </div>
         </div>
-        <div className="flex flex-wrap mt-3 bg-white col-span-2 rounded-full p-3 justify-between border-4 border-blue-100">
+        <div className="flex flex-wrap mt-3 bg-white col-span-2 rounded-full p-3 justify-between border-4 border-blue-100 hover:bg-slate-200">
             <div className="flex flex-wrap pl-5">
                 <SiNextdotjs size={32} color="orange" />
                 <p className="font-['Rajdhani'] text-lg font-bold text-blue-950 pl-3">Next Js</p>
             </div> 
             <div className="flex pr-5">
                 <a href="https://www.youtube.com/watch?v=NBZ9Ro6UKV8&list=PLFIM0718LjIVuONHysfOK0ZtiqUWvrx4F&ab_channel=WebProgrammingUNPAS" className="flex">
-                    <button className="bg-gray-400 hover:bg-gray-250 text-white text-sm font-bold py-2 px-4 rounded-full">
+                    <button className="bg-gray-400 hover:bg-blue-900 text-white text-sm font-bold py-2 px-4 rounded-full">
                         Indonesia
                     </button>                      
                 </a>   
                 <a href="https://www.youtube.com/watch?v=NBZ9Ro6UKV8&list=PLFIM0718LjIVuONHysfOK0ZtiqUWvrx4F&ab_channel=WebProgrammingUNPAS" className="flex pl-2">
-                    <button className="bg-gray-400 hover:bg-gray-250 text-white text-sm font-bold py-2 px-4 rounded-full">
+                    <button className="bg-gray-400 hover:bg-blue-900 text-white text-sm font-bold py-2 px-4 rounded-full">
                         English
                     </button>                      
                 </a>   
